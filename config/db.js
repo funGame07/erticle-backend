e12267d3bd9c33542842da91ab9c0af6ee8c0be8
elbert <elbertchen007@gmail.com>
2025-07-13 18:29:25 +0700

    add new route

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 async function db(){
     await mongoose.connect(
-        'mongodb+srv://elbert:passwords@cluster0.x56s9nj.mongodb.net/erticle?retryWrites=true&w=majority&appName=Cluster0'
+        // 'mongodb+srv://elbert:passwords@cluster0.x56s9nj.mongodb.net/erticle?retryWrites=true&w=majority&appName=Cluster0'
+        process.env.MONGODB_URI
     )
 }
 
