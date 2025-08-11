27700b38399009ff0308131e7a842bbffb3ece49
FunGame <161847205+funGame07@users.noreply.github.com>
2025-08-11 08:02:33 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function db(){
     await mongoose.connect(
         // 'mongodb+srv://elbert:passwords@cluster0.x56s9nj.mongodb.net/erticle?retryWrites=true&w=majority&appName=Cluster0'
-        process.env.MONGODB_URI
+        process.env.DB_URI
     )
 }
 
